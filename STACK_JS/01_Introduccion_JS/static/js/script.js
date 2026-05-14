54c6c562e47f200a694000177e0189cff75ac459
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-05-14 12:55:08 -0400

    ok

--- STACK_JS/01_Introduccion_JS/static/js/script.js
+++ STACK_JS/01_Introduccion_JS/static/js/script.js
@@ -3,8 +3,7 @@ function cambiarTexto() {
     let titulo = "Hola, bienvenido"
     if (document.getElementById("titulo").innerText == titulo) {
         document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
-    }else{
-                document.getElementById("titulo").innerText = "Hola, bienvenido";
-
+    } else {
+        document.getElementById("titulo").innerText = "Hola, bienvenido";
     }
 }
