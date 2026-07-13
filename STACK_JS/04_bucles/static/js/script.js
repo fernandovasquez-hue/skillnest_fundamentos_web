a2e4021beff058fa1d38b0548e98c07cf0bc3552
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-07-13 16:09:38 -0400

    ok

--- STACK_JS/04_bucles/static/js/script.js
+++ STACK_JS/04_bucles/static/js/script.js
@@ -1,8 +1,20 @@
 console.log(`conexión exitsa`)
 /* Ejemplo Bucle for*/
-function contarHasta2(){
-for (let i = 0; i < 11; i++) {
-    alert(i);
+function contarHasta2() {
+    for (let i = 0; i <= 5; i++) {
+        alert(i);
+    }
 }
+function cuentaRegresiva() {
+    for (let i = 10; i > 0; i--) {
+        alert(i);
+    }
+}
+let usuarios = ["Ana", "Luis", "María","Fernando","Xavier"];
+
+function recorrerLista() {
+    for (let i = 0; i < usuarios.length; i++) {
+        alert("Bienvenido, " + usuarios[i]);
+    }
 }
 /* Ejemplo Bucle while*/ 
