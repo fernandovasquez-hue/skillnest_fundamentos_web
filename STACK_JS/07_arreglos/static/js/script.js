f58050eab759016871ec477f74e3e57b98045f61
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-07-15 09:25:58 -0400

    ok

---
+++ STACK_JS/07_arreglos/static/js/script.js
@@ -0,0 +1,16 @@
+console.log(`conexión exitosa`)
+
+//Variables a lista
+
+let nombre1 = "Ana"
+let nombre2 = "Pedro"
+let nombre3 = "María"
+let nombre4 = "Carlos"
+
+//Transformar en array y mostrar
+
+let nombres = ["Ana", "Pedro", "María", "Carlos"]
+
+//Mostrar a María en una alerta
+
+alert(nombres[2]);
