2069ddc8e183fbd6a0ead8ca8b84a24854ea6fd9
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-07-27 09:16:53 -0400

    ok

--- STACK_JS/09_objetos_JS/static/js/script.js
+++ STACK_JS/09_objetos_JS/static/js/script.js
@@ -1,51 +1,67 @@
 console.log(`conexión exitsa`)
 
-
-let hamburguesaEspecial = {
-    pan: "Pan brioche",
-    carne: "Pollo crujiente",
-    queso: "Suizo",
-    extras: [
-        "Lechuga",
-        "Pepinos",
-        "Miel"
-    ],
-    mostrarIngredientes: function () {
-        console.log("Pan:", this.pan);
-        console.log("Carne:", this.carne);
-        console.log("Queso:", this.queso);
-        console.log(
-            "Extras:",
-            this.extras.join(", ")
-        );
-    }
-};
-//Accede al pan
-console.log(hamburguesaEspecial.pan)
-hamburguesaEspecial.mostrarIngredientes();
-
+function hamburguesaEspecial() {
+    let hamburguesaEspecial = {
+        pan: "Pan brioche",
+        carne: "Pollo crujiente",
+        queso: "Suizo",
+        extras: [
+            "Lechuga",
+            "Pepinos",
+            "Miel"
+        ],
+        mostrarIngredientes: function () {
+            alert(`Pan: ${this.pan}
+            \nCarne: ${this.carne}
+            \nQueso: ${this.queso}
+                \nExtras: ${this.extras.join(", ")}`
+            );
+        }
+    };
+    //Accede al pan
+    console.log(hamburguesaEspecial.pan)
+    hamburguesaEspecial.mostrarIngredientes();
+}
 //Metodo del auto
-let auto = {
-    marca:"Toyota",
-    modelo:"Corolla",
-    año:2023,
-    encender:function(){
-        console.log(
-            "Encendiendo",
-            this.marca,
-            this.modelo
-        );
-    }
-};
+function piezasDeAuto() {
+    let auto = {
+        auto: "Honda Civic",
+        modelo: "EJ1 Coupé (1993)",
+        motor: "Spoon Engines",
+        turbo: "T66 Turbo",
+        oxidoNitroso: "NOS",
+        escape: "Motec system exhaust",
+        extras: [
+            "Wings West",
+            "Aleron aluminio estilo R33 GT-R",
+            "LLantas Axis Se7ven de 17 o 18 pulgadas",
+            "Suspensión TEIN Flex Z/ Street Advance Z"
+        ],
+        piezasDeAuto: function () {
+            alert(
+                `Auto: ${this.auto}
+            Motor: ${this.modelo}
+            Turbo: ${this.turbo}
+            Oxído Nitroso: ${this.oxidoNitroso}
+            Escape: ${this.escape}
+            Extras: ${this.extras.join(" - ")}`
+            );
+        }
+    };
+    auto.piezasDeAuto()
+}
 
 //Metodo de la casa
-let casa = {
-    direccion:"Av. Siempre Viva 742",
-    habitaciones:4,
-    baños:2,
-    mostrarInformacion:function(){
-        console.log(
-            this.direccion
-        );
-    }
-};
+function mostrarInformacionCasa() {
+    let casa = {
+        direccion: "Av. Siempre Viva 742",
+        habitaciones: 4,
+        baños: 2,
+        mostrarInformacion: function () {
+            alert(`Dirección: ${this.direccion}
+            \nHabitaciones: ${this.habitaciones}
+            \nBaños: ${this.baños}`)
+        }
+    };
+    casa.mostrarInformacion();
+}
