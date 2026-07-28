ad9131eccf869d90189d9c9f899b236e5cc54729
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-07-28 11:15:02 -0400

    ok

--- STACK_JS/11_funciones_JS/static/js/script.js
+++ STACK_JS/11_funciones_JS/static/js/script.js
@@ -1,1 +1,24 @@
 console.log(`conexión exitsa`)
+// Ejemplo  Función simple (sin parametros)
+function saludar(/*parametros*/) {
+    alert("¡Hola, bienvenido!");
+}
+saludar();//Ejecución de una función
+saludar();
+saludar();
+function saludar(nombre) {
+    console.log("¡Hola, " + nombre + "!");
+}
+saludar("Luis");
+saludar("Ana");
+let numero1 = 10;
+let numero2 = 7;
+let maximo = encontrarMaximo(numero1, numero2);
+console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
+function encontrarMaximo(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
