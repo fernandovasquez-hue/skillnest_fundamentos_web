d17c00c86b9b4e7b3dd399aff699c10ab8b6d4e8
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-07-28 15:15:31 -0400

    ok

--- STACK_JS/11_funciones_JS/static/js/script.js
+++ STACK_JS/11_funciones_JS/static/js/script.js
@@ -4,17 +4,16 @@ function saludar(/*parametros*/) {
     alert("¡Hola, bienvenido!");
 }
 saludar();//Ejecución de una función
-saludar();
-saludar();
-function saludar(nombre) {
-    console.log("¡Hola, " + nombre + "!");
+// saludar();
+// saludar();
+function saludarParam(nombre) {
+    alert(`¡Hola, ${nombre}!`);
 }
-saludar("Luis");
-saludar("Ana");
-let numero1 = 10;
-let numero2 = 7;
-let maximo = encontrarMaximo(numero1, numero2);
-console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
+saludarParam("Fernando");
+saludarParam("Estefano");
+
+// 🎯 Funciones con return
+
 function encontrarMaximo(a, b) {
     if (a > b) {
         return a;
@@ -22,3 +21,22 @@ function encontrarMaximo(a, b) {
         return b;
     }
 }
+
+let numero1 = 10;
+let numero2 = 7;
+let maximo = encontrarMaximo(numero1, numero2);
+alert(`El número mayor entre ${numero1} y ${numero2} es: ${maximo}`);
+
+// Tarea
+/*
+Crear una funcion que reciba 3 parametros,a b y c.
+Debe sumar a + b y elultimo resutado restarlo por c.
+Devolver el valor final y mostrar con un alert
+*/
+let numeroA = parseInt(prompt(`Ingrese primer valor:`));
+let numeroB = parseInt(prompt(`Ingrese segundo valor:`))
+let numeroC = parseInt(prompt(`Ingrese tercer valor:`))
+function ecuacionMatematica(a, b, c){
+return a + b - c
+}
+
