0f853b6050192b3cff6a79a016f232626a8aba78
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-08-03 08:55:58 -0400

    ok

--- STACK_JS/12_interacion_click/static/js/script.js
+++ STACK_JS/12_interacion_click/static/js/script.js
@@ -4,7 +4,6 @@ document.getElementById("colorButton1").addEventListener("click", function () {
     cambiarColor();
 });
 
-// Función que cambia el color de fondo al hacer clic
 function cambiarColor() {
     let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
@@ -19,22 +18,16 @@ function cambiarTextoColor() {
     let colorAleatorioTexto = coloresTexto[Math.floor(Math.random() * coloresTexto.length)];
     document.body.style.color = colorAleatorioTexto;
 }
-// Seleccionamos el botón por su id
 let boton = document.getElementById("cambiarTexto");
 
-// Agregamos un evento 'click' al botón
 boton.addEventListener("click", function () {
-    // Cambia el texto del botón usando `this`
     this.innerText = "¡Texto cambiado!";
 });
 
-// Seleccionamos todas las imágenes con la clase 'imagenEliminar'
 let imagenes = document.querySelectorAll(".imagenEliminar");
 
-// Agregamos un evento 'click' a cada imagen
 imagenes.forEach(function (imagen) {
     imagen.addEventListener("click", function () {
-        // Eliminamos la imagen al hacer clic
         this.remove();
     });
 });
