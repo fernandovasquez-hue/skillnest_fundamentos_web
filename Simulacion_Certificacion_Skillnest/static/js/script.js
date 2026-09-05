67cb396660edaa34ed6712da992124eba0254c4f
fernandovasquez-hue <fernandovasquez@liceovvh.cl>
2026-09-04 20:35:35 -0400

    ok

--- Simulacion_Certificacion_Skillnest/static/js/script.js
+++ Simulacion_Certificacion_Skillnest/static/js/script.js
@@ -10,8 +10,25 @@ cambioImagen.addEventListener("mouseout", function () {
     cambioImagen.src = "static/image/comida-mexicana.jpg"
 })
 
-function Bienvenido() {
-    const texto = document.getElementById(`correo`).value;
+const botonLogin = document.querySelector("#botonLogin")
+const correoIngresado = document.querySelector("#correo")
 
-    alert("¡Bienvenido! Tu correo es: " + texto);
-}
+botonLogin.addEventListener("click", function(){
+    alert(`Bienvenid@ ${correoIngresado.value}`)
+})
+
+const botonAgregar = document.querySelector("#boton")
+const botonAgregar2 = document.querySelector("#boton2")
+
+const contadorPedidos = document.querySelector("#pedidos")
+
+botonAgregar.addEventListener("click", function(){
+    let i = parseInt(contadorPedidos.innerText);
+        i++;
+        contadorPedidos.innerText = `${i}`;
+})
+botonAgregar2.addEventListener("click", function(){
+    let i = parseInt(contadorPedidos.innerText);
+        i++;
+        contadorPedidos.innerText = `${i}`;
+})
